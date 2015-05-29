a3481bc6a9b471d61ec942a2b8fce107de4b7fab
Stephanie <stephaniebarreyro@gmail.com>
2015-05-29 05:15:37 -0700

    Reverse db migrations.

--- db/migrate/20150529113948_create_user_values.rb
+++ db/migrate/20150529113948_create_user_values.rb
@@ -1,8 +1,13 @@
 class CreateUserValues < ActiveRecord::Migration
   def change
     create_table :user_values do |t|
+      t.references :user, index: true
+      t.references :value, index: true
+      t.integer :count
 
-      t.timestamps
+      t.timestamps null: false
     end
+    add_foreign_key :user_values, :users
+    add_foreign_key :user_values, :values
   end
 end
--- db/migrate/20150529113955_create_values.rb
+++ db/migrate/20150529113955_create_values.rb
@@ -1,13 +1,9 @@
-class CreateUserValues < ActiveRecord::Migration
+class CreateValues < ActiveRecord::Migration
   def change
-    create_table :user_values do |t|
-      t.references :user, index: true
-      t.references :value, index: true
-      t.integer :count
+    create_table :values do |t|
+      t.string :word
 
       t.timestamps null: false
     end
-    add_foreign_key :user_values, :users
-    add_foreign_key :user_values, :values
   end
 end
